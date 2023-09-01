b3705452d0c8a47d9bd0487745980e9f91d04ae5
LuckyGUY434 <ivanstjepan007@gmail.com>
2023-09-02 00:39:59 +0200

    finished working on basics of game.

--- index.js
+++ index.js
@@ -1,56 +1,84 @@
+let userScore = 0
+let computerScore = 0
+let matchResult = ''
 
-function game(){
+document.addEventListener('DOMContentLoaded', () => {
+const buttons = document.querySelectorAll('.container button')
+const userResult = document.querySelector('.userResult h3')
 
-let userScore = '0'
-let computerScore = '0'
+const computerResult = document.querySelector('.userResult .computerResult')
+const roundInfo = document.querySelector('.roundResult h3')
 
+
+const winResultElement = document.querySelector('.winResult h1');
+const loseResultElement = document.querySelector('#loseResult')
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const userChoice = button.textContent.toLowerCase();
+        const computerChoice = getComputerChoice();
+        playRound(userChoice, computerChoice);
+        endMatch();
+    });
+});
 
 function playRound(playerSelection, computerSelection){
+    let roundResult = ''
+
     if(playerSelection == 'rock' && computerSelection == 'paper'){
-        return console.log('You lose, paper bears rock.'), computerScore++
-    }else if(playerSelection == 'rock' && computerSelection == 'cissor'){
-        return console.log('You won, rock beats cissor.'), userScore++
-    }else if(playerSelection == 'paper' && computerSelection=='cissor'){
-        return console.log('You lose, cissor beats paper'), computerScore++
+    roundResult = 'You lost this round, paper beats rock!' 
+    computerScore++
+    }else if(playerSelection == 'rock' && computerSelection == 'scissor'){
+    roundResult = 'You won this round, rock beats scissor!' 
+    userScore++
+    }else if(playerSelection == 'paper' && computerSelection=='scissor'){
+        roundResult = 'You lost this round, scissor beats paper!' 
+        computerScore++
     }else if(playerSelection == 'paper' && computerSelection=='rock'){
-        return console.log('You won, paper beats rock'), userScore++
-    }else if(playerSelection=='cissor' && computerSelection=='rock'){
-        return console.log('You lose, rock beats cissor'), computerScore++
-    }else if(playerSelection =='cissor' && computerSelection=='paper'){
-        return console.log('You won, cissor beats paper'), userScore++
+        roundResult = 'You won this round, paper beats rock!'
+         userScore++
+    }else if(playerSelection=='scissor' && computerSelection=='rock'){
+        roundResult = 'You lost this round, rock beats scissor!' 
+        computerScore++
+    }else if(playerSelection =='scissor' && computerSelection=='paper'){
+        roundResult = 'You won this round, scissor beats paper!'
+        userScore++
     }else{
-        return console.log('Even')
+        roundResult = 'This round is Even!'
     }
+    userResult.textContent = `Your score is: ${userScore}`
+    computerResult.textContent = `Computer socer is: ${computerScore}`
+    roundInfo.textContent = roundResult
+    console.log(`Your score is ${userScore}`);
+    console.log(`Computer score is ${computerScore}`);
 }
 
-while(userScore < 5 && computerScore < 5){
 
-const playerSelection = prompt('paper, cissor or rock: ')
-if(playerSelection === null){
-    alert('canceled');
-    break;
-}
-const computerSelection = getComputerChoice();
 
-playRound(playerSelection, computerSelection)
-
-console.log(`your score is ${userScore}`)
-console.log(`computer score is ${computerScore}`)
-
-}
-if(userScore > computerScore){
-    console.log(`YOU WIN!    your score: ${userScore} and computer score: ${computerScore}`)
-}else if(userScore < computerScore){
-    console.log(`YOU LOSE!   your score: ${userScore} and computer socre: ${computerScore}`)
-}else{
-    console.log(`ITS TIE!    your score: ${userScore} and computer score: ${computerScore}`)
-
-}
-}
 function getComputerChoice(){
-    const choices = ['rock','paper','cissor'];
+    const choices = ['rock','paper','scissor'];
     const randomElement = choices[Math.floor(Math.random() * choices.length)];
     return randomElement;
 }
 
-console.log(game())
+function endMatch() {
+
+    if (userScore === 5) {
+        win = 'You won';
+        userScore = 0;
+        computerScore = 0;
+        winResultElement.textContent = win 
+    } else if (computerScore === 5) {
+        lose = 'You lose';
+        userScore = 0;
+        computerScore = 0;
+        loseResultElement.textContent = lose 
+    }
+    else {
+        winResultElement.textContent = ''; 
+        loseResultElement.textContent = '';
+    }
+}
+
+})
